14b047d98a570d5577b9b17986e79506506fb9e1
yassineS <yassinesouilmi@gmail.com>
2015-09-27 00:57:14 +0100

    Edit the deploy script -untested-

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,9 +10,9 @@ if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
   git add .
   git commit -m "Deploy"
 
-  # We redirect any output to
-  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+
+  git remote add origin https://github.com/yassineS/yassines.github.io.git
+  git push --force --quiet origin master
 else
   echo 'Invalid branch. You can only deploy from gh-pages.'
   exit 1
